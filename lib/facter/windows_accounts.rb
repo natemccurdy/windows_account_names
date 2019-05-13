8af2aeea516edb45018c6cc9afb0e4ff0b371362
Nate McCurdy <mccurdy.nate@gmail.com>
2019-05-13 13:50:42 -0700

    Use Puppet::Resource instead of Puppet::Type to find all users
    This changes which API is used to find all user accounts. The
    Puppet::Type.type() call was a little funky to work with and seemed to
    sit a rather low level of abstraction.
    
    The Puppet::Resource.indirection.search() call is used by the 'puppet
    resource' command and seems to be at a higher level of abstraction. It
    also has outputs that are easier to parse.
    
    https://github.com/puppetlabs/puppet/blob/6.4.2/lib/puppet/application/resource.rb#L228
    
    Additionally, this change gets rid of the improper use of `find_all()`.
    We now search through the list of users with `find()` which has the
    benefit of stopping the search once a match is found.

--- lib/facter/windows_accounts.rb
+++ lib/facter/windows_accounts.rb
@@ -11,13 +11,10 @@ Facter.add(:windows_accounts) do
   setcode do
     account_names = {}
 
-    Puppet::Type.type('user').instances.find_all do |user|
-      user_values = user.retrieve
-      # Check for the well-known admin SID.
-      account_names['Administrator'] = user.name if user_values[user.property(:uid)] =~ /^S-1-5-21.*-500$/
-      # Check for the well-known guest SID.
-      account_names['Guest'] = user.name if user_values[user.property(:uid)] =~ /^S-1-5-21.*-501$/
-    end
+    all_users = Puppet::Resource.indirection.search('user')
+
+    account_names['Administrator'] = all_users.find { |user| user[:uid] =~ /^S-1-5-21.*-500$/ }.title
+    account_names['Guest']         = all_users.find { |user| user[:uid] =~ /^S-1-5-21.*-501$/ }.title
 
     account_names
   end
